750e45ea08881da15c207e40e12d23b5154ae84b
Daminichandrakar <damini.2201@gmail.com>
2021-09-10 00:23:06 +0530

    Added: FirstName Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,22 @@
 package com.bl;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-      public static void main(String[] args) {
-		System.out.println("Welcome message");
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first name");
+		String firstName = scan.nextLine();
+		UserRegistration userRegistration = new UserRegistration();
+		userRegistration.nameValidation(firstName);
 	}
-} 
+
+	public void nameValidation(String fname) {
+		String regex = "^[A-Z][a-z]{2,}"; // name validation name start with cap and contain 3 characters
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(fname);
+		System.out.println("is valid: " + matcher.matches());
+	}
+}
