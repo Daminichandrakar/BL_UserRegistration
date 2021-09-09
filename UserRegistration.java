10d618a805aa20d5a1016cb60360a3ac804a2e13
Daminichandrakar <damini.2201@gmail.com>
2021-09-10 00:57:50 +0530

    Added: Password validation conatin 1 Sprcial Character

--- UserRegistration.java
+++ UserRegistration.java
@@ -55,8 +55,9 @@ public class UserRegistration {
 	}
 
 	public void passwordValidation(String password) {
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])[a-z0-9]+.{8,}$"; // password validation
+		String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@+.-]).{8,20}$"; // password validation
 		Pattern pattern = Pattern.compile(passwordRegex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println("is valid password: " + matcher.matches());
-	}}
+	}
+}
