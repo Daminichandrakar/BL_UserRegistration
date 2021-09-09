58e40dd3532465af98ec699e2006db6e71c42863
Daminichandrakar <damini.2201@gmail.com>
2021-09-10 00:42:17 +0530

    Added: Password validation conatin 1 special character

--- UserRegistration.java
+++ UserRegistration.java
@@ -55,7 +55,7 @@ public class UserRegistration {
 	}
 
 	public void passwordValidation(String password) {
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])[a-z]+.{8,20}$"; // password validation
+		String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@+.-]).{8,20}$"; // password validation
 		Pattern pattern = Pattern.compile(passwordRegex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println("is valid password: " + matcher.matches());
