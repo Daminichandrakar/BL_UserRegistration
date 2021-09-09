e7f6e63bb7a834b7e0c38b344ad359560d9298aa
Daminichandrakar <damini.2201@gmail.com>
2021-09-10 00:50:34 +0530

    Added: Password validation conatin min 1 UpeerCase

--- UserRegistration.java
+++ UserRegistration.java
@@ -55,9 +55,8 @@ public class UserRegistration {
 	}
 
 	public void passwordValidation(String password) {
-		String passwordRegex = "^[A-Za-z0-9]{8,20}"; // password validation
+		String passwordRegex = "^(?=.*[A-Z])[a-z0-9]+.{8,20}$"; // password validation
 		Pattern pattern = Pattern.compile(passwordRegex);
 		Matcher matcher = pattern.matcher(password);
 		System.out.println("is valid password: " + matcher.matches());
-	}
-}
+	}}
